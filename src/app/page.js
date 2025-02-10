17515962ba5f1d730d275e1319e26dece6e8cd87
moonscape09 <munim.adil@outlook.com>
2025-02-09 21:12:01 -0500

    Add disclaimer note

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default async function App() {
 
   return (
     <main className='w-full'>
+      <text>CURRENTLY UNDER DEVELOPMENT: YOU ARE VIEWING OUR MVP (no Safari support yet)</text>
       <HomePage />
     </main>
   );
