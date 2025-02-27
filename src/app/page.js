e7cf576340adcd8561aab6e363fbb45868a875e7
moonscape09 <munim.adil@outlook.com>
2025-02-27 15:45:39 -0500

    change browser support message

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HomePage from "@/components/HomePage";
 export default async function App() {
   return (
     <main className='w-full'>
-      <text>CURRENTLY UNDER DEVELOPMENT: YOU ARE VIEWING OUR MVP (Only Chrome support so far)</text>
+      <text>CURRENTLY UNDER DEVELOPMENT: YOU ARE VIEWING OUR MVP (only Chrome support so far)</text>
       <HomePage />
     </main>
   );
