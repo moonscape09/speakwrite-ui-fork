4e667dccacd7c555afc103af33efa3538c95a839
ahmedz45 <zuhayr.ahmed@mail.utoronto.ca>
2025-03-31 15:48:09 -0400

    [DEV 4.0] fixed microphone carrying over different sessions

--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -24,6 +24,7 @@ export default function TextBlock({ onClose, setFileTitle, currentFileID, trigge
   const [MediaCounter, setMediaCounter] = useState(0);
   // const [transcription, setTranscription] = useState("");
   // const [pdfContent, setPdfContent] = useState("");
+  let recognition = null; // Declare recognition outside of the component to avoid re-initialization
 
   const pdfContentRef = useRef("");
   const transcriptionRef = useRef("");
@@ -95,6 +96,14 @@ export default function TextBlock({ onClose, setFileTitle, currentFileID, trigge
       setTitle(fetched_session.session_name);
       update_chat_history("");
       setMediaCounter(0);
+      setIsConnected(false);
+      if (wsRef.current) {
+        wsRef.current.close();
+      }
+      if (recognition) {
+        recognition.stop(); // Stop speech recognition when disconnecting
+      }
+      setIsConnected(false);
       pdfContentRef.current = "";
       transcriptionRef.current = "";
       contentRef.current.value = fetched_session.context.message || ""; // if still undefined then it'll just be an empty string
@@ -108,7 +117,7 @@ export default function TextBlock({ onClose, setFileTitle, currentFileID, trigge
 
   // Handle WebSocket connection
   const handleStartButtonClick = (tone) => {
-    const recognition = setUpRecognition(
+    recognition = setUpRecognition(
       wsRef,
       c_sid,
       pdfContentRef,
